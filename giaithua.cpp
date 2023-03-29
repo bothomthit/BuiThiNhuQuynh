a3d72f5dd954e06392889558f304d9c422991b59
bothomthit <125108405+bothomthit@users.noreply.github.com>
2023-03-29 15:30:40 +0700

    Create giaithua.cpp

---
+++ giaithua.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+int giaithua(int n){
+  printf("nhap vao n");
+  scanf("%d" &n);
+  if(n==0)
+    return 0;
+  if(n==1)
+    return 1;
+  return n*giaithua(n-1);
+}
+int x;
+prinft("nhap vao x");
+scanf("%d" &x);
+int TinhS(x,n)
+{
+  white(int n=1){
+    S = S + x^n*giaithua(n-1);
+    n++;
+  } 
+  return S;
+  
+}}
+
