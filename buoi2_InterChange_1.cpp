1f7f861608eab02050e05aeb6348d021d1d7b627
bothomthit <125108405+bothomthit@users.noreply.github.com>
2023-04-05 15:04:34 +0700

    Create buoi2_InterChange_1.cpp

---
+++ buoi2_InterChange_1.cpp
@@ -0,0 +1,15 @@
+void Sapxep(int a[], int n)
+{
+	for (int i = 0; i < n-1;i++)
+		for (int j = i + 1; j < n;j++)
+		if (a[i]>a[j])
+			swap(a[i], a[j]);
+}
+
+
+void swap(int &a, int &b)
+{
+	int x = a;
+	a = b;
+	b = x;
+}
