66865a675a749ed63d71cf3d629da179195f8507
bothomthit <125108405+bothomthit@users.noreply.github.com>
2023-04-05 15:01:07 +0700

    Create buoi2_Linenear_1.cpp

---
+++ buoi2_Linenear_1.cpp
@@ -0,0 +1,12 @@
+int LinearSearch(int a[], int n, int x){
+	int i=0; a[n]=x; //a[n] là phần tử “lính canh”
+	while(a[i]!=x){
+		i++;
+	}
+	if(i==n){
+		return -1; //Tìm không thấy x
+	}
+	else{
+		return i; //Tìm thấy x
+	}
+}
